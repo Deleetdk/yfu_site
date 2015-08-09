2cab98e4a7b61e2da3653a6686058d2cdf5abd8d
Emil Kirkegaard <emil@emilkirkegaard.dk>
2015-08-09 15:03:29 -0500

    removed secret key

--- yfu_site/settings.py
+++ yfu_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-(7kug+hj!xh9fd_afv4un^1r0qf75lz!r329+_7^rwz-xk)tb'
+SECRET_KEY = '' #change this
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
